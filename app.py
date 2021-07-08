1bd5ff95bdee8fc5d386bd29bae36903cad2d6fa
Belal Hamdy <belalhezzat@gmail.com>
2021-07-09 01:26:02 +0200

    Added precision

--- app.py
+++ app.py
@@ -35,13 +35,17 @@ def predict():
 	data = request.get_json()
 	text = data.get('text', '')
 	threshold = data.get('threshold',0)
+	precision = data.get('precision',3)
 	if threshold > 1 or threshold < 0:
-	  threshold = 0
+		threshold = 0
+  	if precision < 0:
+		precision = 3
 	cleaned = clean_text(text)
 	prediction = model.predict(cleaned)
 	preds_df = pd.concat([pd.DataFrame(labels),pd.DataFrame(prediction[2])],axis=1)
 	preds_df.columns = ["Label", "Probability"]
 	filtered_df = preds_df[preds_df["Probability"] >= threshold]
+	filtered_df['Probability'] = filtered_df['Probability'].round(decimals=precision)
 	preds_dict = dict(zip(filtered_df.Label, filtered_df.Probability))
 	preds_dict_sorted = sorted(preds_dict.items(), key=lambda x: x[1], reverse=True)
-	return json.dumps({'input':text,'cleaned':cleaned,'threshold':threshold,'prediction': OrderedDict(preds_dict_sorted)})
+	return json.dumps({'input':text,'cleaned':cleaned,'threshold':threshold,'precision':precision,'prediction': OrderedDict(preds_dict_sorted)})
